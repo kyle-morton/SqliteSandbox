c9b200978793da2940d261e3ddf72151627a1f95
kylemorton5770 <kylemortondevelopment@gmail.com>
2018-03-29 09:39:23 -0500

    added dapper to convert datareader from sqlite to classes

---
+++ SqliteEFDemo/Models/Tracks.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqliteEFDemo.Models
+{
+    public class Track
+    {
+        public int TrackId { get; set; }
+        public string Name { get; set; }
+        public int AlbumId { get; set; }
+        public int MediaTypeId { get; set; }
+        public int GenreId { get; set; }
+        public string Composer { get; set; }
+        public int Milliseconds { get; set; }
+        public int Bytes { get; set; }
+        public decimal UnitPrice { get; set; }
+
+        //public object TrackId { get; set; }
+        //public object Name { get; set; }
+        //public object AlbumId { get; set; }
+        //public object MediaTypeId { get; set; }
+        //public object GenreId { get; set; }
+        //public object Composer { get; set; }
+        //public object Milliseconds { get; set; }
+        //public object Bytes { get; set; }
+        //public object UnitPrice { get; set; }
+
+    }
+}
--- SqliteEFDemo/Services/SqlLiteConnService.cs
+++ SqliteEFDemo/Services/SqlLiteConnService.cs
@@ -5,6 +5,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SQLite;
+using SqliteEFDemo.Models;
+using System.Data;
+using Dapper;
 
 namespace SqliteEFDemo
 {
@@ -12,23 +15,35 @@ namespace SqliteEFDemo
     {
         private string _connectionString => ConfigurationManager.AppSettings["connectionString"];
 
-        public void GetData()
+        public List<Track> GetData()
         {
+            var list = new List<Track>();
             try
             {
                 using (SQLiteConnection conn = new SQLiteConnection(_connectionString))
                 {
                     conn.Open();
-                    string sql = "SELECT * FROM employees; ";
+                    //string sql = "SELECT * FROM tracks; ";
+                    string sql = "SELECT TrackId, Name, AlbumId, MediaTypeId, GenreId, Composer, Milliseconds, Bytes, UnitPrice FROM tracks;";
+
+                    var results = conn.Query<Track>(sql).ToList();
 
                     using (SQLiteCommand cmd = new SQLiteCommand(sql, conn))
                     {
                         using (SQLiteDataReader reader = cmd.ExecuteReader())
                         {
-                            while (reader.Read())
+                            if (reader.HasRows)
                             {
-                                var EmployeeId = reader["EmployeeId"].ToString();
-                                var LastName = reader["LastName"].ToString();
+                                var records = reader.Cast<IDataRecord>().ToList();
+                                list = MappingUtil.ToTracks(records);
+                            }
+                            if (reader.HasRows)
+                            {
+                                while (reader.Read())
+                                {
+                                    var EmployeeId = reader["TrackId"].ToString();
+                                    var LastName = reader["Name"].ToString();
+                                }
                             }
                         }
                     }
@@ -39,7 +54,11 @@ namespace SqliteEFDemo
             {
                 Console.WriteLine("Ex: " + e);
             }
+
+            return list;
         }
+
+
 
     }
 }
