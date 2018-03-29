de44627b07e8ec0c31548c6a03a9e2ef731b1a38
kylemorton5770 <kylemortondevelopment@gmail.com>
2018-03-28 22:19:50 -0500

    Add project files.

---
+++ SqliteEFDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqliteEFDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new SqlLiteConnService().GetData();
+            Console.WriteLine("After Data...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SqliteEFDemo/Service/SqlLiteConnService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace SqliteEFDemo
+{
+    public class SqlLiteConnService
+    {
+        private string _connectionString => ConfigurationManager.AppSettings["connectionString"];
+
+        public void GetData()
+        {
+            try
+            {
+                using (SQLiteConnection conn = new SQLiteConnection(_connectionString))
+                {
+                    conn.Open();
+                    string sql = "SELECT * FROM employees; ";
+
+                    using (SQLiteCommand cmd = new SQLiteCommand(sql, conn))
+                    {
+                        using (SQLiteDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                var EmployeeId = reader["EmployeeId"].ToString();
+                                var LastName = reader["LastName"].ToString();
+                            }
+                        }
+                    }
+                    conn.Close();
+                }
+            }
+            catch (SQLiteException e)
+            {
+                Console.WriteLine("Ex: " + e);
+            }
+        }
+
+    }
+}
